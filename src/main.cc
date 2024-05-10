64cf83e34e083398033a70a38290f127e89f4ebb
EvanMaxted <evanmaxted@cmail.carleton.ca>
2024-05-10 15:26:38 +0000

    Test commit

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world! Here is a change I am testing out" << std::endl;
     return 0;
 }
